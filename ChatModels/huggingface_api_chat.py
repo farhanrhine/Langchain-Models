17e69a94ba6c08a11cc961f6be4e7f9d0484af03
farhanrhine <mohammadfarhanalam09@gmail.com>
2025-05-26 16:06:47 +0530

    working huggingface api

---
+++ ChatModels/huggingface_api_chat.py
@@ -0,0 +1,16 @@
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("why was war in israel and palestine and who is responsible for it?")
+print(result.content)
+
+
